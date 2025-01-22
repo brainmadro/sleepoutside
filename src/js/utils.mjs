5147f300dc157d3d99b1d254e05cb5eb06991496
henryflia <114320959+Henryflia@users.noreply.github.com>
2025-01-22 13:20:38 -0400

    feat: ✨ W02 Individual and some of group activity (#4)
    * MY team
    
    * my work
    
    * Create index.html
    
    * Create ProductDetails.mjs
    
    New File JS
    
    * Update index.html
    
    * number5
    
    * my work
    
    * Workin
    
    * Update the cart
    
    * zinnecesary code
    
    * revert: :rewind: Revert change on structure and vite config
    
    * feat: :sparkles: Complete W02 Activity fixing errors on the way
    
    * revert: :rewind: Set comments in utils as it was before
    
    * fix: :bug: Correct script src in main index
    
    ---------
    
    Co-authored-by: aaron-lopez22 <aaron.a.lopez22@gmail.com>
    Co-authored-by: Brian Madroñero <brainmadro@gmail.com>

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -7,12 +7,52 @@ export function qs(selector, parent = document) {
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+  const data = localStorage.getItem(key);
+  if (data) {
+    try {
+      const parsedData = JSON.parse(data);
+      if (Array.isArray(parsedData)) {
+        return parsedData;
+      } else {
+        console.warn(`${key} Error!!!`);
+        return [];
+      }
+    } catch (e) {
+      console.error(`Error parsed ${key}:`, e);
+      return [];
+    }
+  }
+  return [];
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
+// helper to get parameter strings
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
+// function to take a list of objects and a template and insert the objects as HTML into the DOM
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
