8980a169f92689df6da5966e16d20b13c372fb5f
Brian Madroñero <brainmadro@gmail.com>
2025-01-23 19:27:53 -0500

    revert: :rewind: Revert changes in product.js due to conflics with main branch

--- src/js/product.js
+++ src/js/product.js
@@ -1,19 +1,9 @@
-import { getLocalStorage, setLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
 const product = new ProductDetails(productId, dataSource);
 
 product.init();
-
-  const productId = getParams('product'); 
-
-  async function displayProductDetails() {
-    const product = await dataSource.findProductById(productId); 
-  }
-  displayProductDetails();
-
-  document
-  .getElementById("addToCart")
-  .addEv
