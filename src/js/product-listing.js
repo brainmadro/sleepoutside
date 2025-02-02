8e15a5ef74b824ecd7b8ce54fc4a6b3f6defdd28
Brian Madroñero <63459127+brainmadro@users.noreply.github.com>
2025-02-01 21:41:59 -0500

    feat: :sparkles: Take data from API and add new page for listing products (#9)
    * feat: :sparkles: Take data from API and add new page for listing products
    
    * chore: :closed_lock_with_key: Update env sample for Netlify

---
+++ src/js/product-listing.js
@@ -0,0 +1,13 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter, getParam } from './utils.mjs';
+
+const category = getParam('category');
+const dataSource = new ProductData();
+const title = document.querySelector("#category-title");
+const listElement = document.querySelector(".product-list");
+const listing = new ProductList(category, dataSource, listElement);
+
+loadHeaderFooter();
+title.textContent = category.replace(/-/g, ' ').replace(/\b\w/g, c => c.toUpperCase());
+listing.init();
