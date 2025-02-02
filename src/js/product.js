c4bfcd8bb8db38150b9679d6c4189700d4e7f221
henryflia <superflia2059@gmail.com>
2025-02-01 23:39:05 -0400

    Step 5 Refactor ProductData

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,11 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
+loadHeaderFooter();
+
+const dataSource = new ExternalServices("tents");
 const productId = getParam("product");
-const dataSource = new ProductData("tents");
 const product = new ProductDetails(productId, dataSource);
 
 loadHeaderFooter();
